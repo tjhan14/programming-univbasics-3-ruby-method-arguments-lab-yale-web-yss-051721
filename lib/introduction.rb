8f1bb9a2757e7766992bd605ee8aa681238a08c5
dependable-coder-f3d7 <tj.han@yale.edu>
2021-05-31 05:20:09 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,6 +7,9 @@ def introduction_with_language_optional(name, language = "Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}"
 end
 
+introduction_with_language_optional("Mike", "Python")
+introduction_with_language_optional("Maria")
+
 
 
 
